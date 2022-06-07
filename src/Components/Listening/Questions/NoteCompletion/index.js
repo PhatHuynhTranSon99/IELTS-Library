d1cbce97d93f13c04b25c782a5f4aa37641aa54b
phat <phat.huynhtranso99@gmail.com>
2022-06-07 21:14:10 +0700

    Styling for header

--- src/Components/Listening/Questions/NoteCompletion/index.js
+++ src/Components/Listening/Questions/NoteCompletion/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import TitleAndTask from "../../../Auxiliary/Question/TitleAndTask";
 import Heading from "../../../Auxiliary/Text/Heading";
+import TextCenter from "../../../Auxiliary/Text/Center";
 import Questions from "./questions";
 
 const NoteCompletion = ({ name, task, heading, questions }) => (
     <React.Fragment>
         <TitleAndTask name={name} task={task}/>
-        <Heading>{ heading }</Heading>
+        <Heading><TextCenter>{ heading }</TextCenter> </Heading>
         <Questions questions={questions}/>
     </React.Fragment>
 );
